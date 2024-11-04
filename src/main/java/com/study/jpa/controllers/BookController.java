2a39b7bb900fa5cdb899d6b45a31ad1fcb44c568
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-11-04 09:00:04 -0300

    method: deletById and findAll

--- src/main/java/com/study/jpa/controllers/BookController.java
+++ src/main/java/com/study/jpa/controllers/BookController.java
@@ -3,13 +3,14 @@ package com.study.jpa.controllers;
 import com.study.jpa.dtos.BookRecordDto;
 import com.study.jpa.entities.Book;
 import com.study.jpa.services.BookService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/bookstore/books")
@@ -24,5 +25,16 @@ public class BookController {
     @PostMapping
     public ResponseEntity<Book> saveBook(@RequestBody BookRecordDto bookRecordDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(bookRecordDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAll(){
+        return ResponseEntity.ok().body(bookService.getAllBooks());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletById(@PathVariable UUID uuid){
+        bookService.deletBook(uuid);
+        return ResponseEntity.noContent().build();
     }
 }
